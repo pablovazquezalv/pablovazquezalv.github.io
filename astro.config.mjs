6d8adc49252226e086c7f431f73d848e379d2b3d
Pablo <pabloalvaradovazquez10@gmail.com>
2023-07-21 10:18:37 -0600

    ss

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://pablovazquezalv.github.io',
-  base : '/portfolio/',
+  base : '/docs/',
   
 });
